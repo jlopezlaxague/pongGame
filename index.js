c04de09a93c0c76d60863902b7ce2092386c819c
Joaquin Lopez Laxague <joaquinlopezlaxague@MacBook-Pro-de-CRUCE.local>
2022-01-02 17:50:33 -0300

    dev

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ const courtWidth = court.clientWidth;
 const playerWidht = right.offsetWidth;
 const playerHeigth = right.offsetHeight;
 const ballSize = ball.offsetWidth;
+const hBallStep = 4;
+const vBallStep = hBallStep / 3;
+const ballTime = 2;
+const playersTime = ballTime * 2;
+const hard = 0.018;
 let computerInterval;
 let ballInterval;
+let playerInterval;
 let stillPlaying = true;
 
 const start = function () {
@@ -19,20 +25,20 @@ const start = function () {
   right.style.left = `${courtWidth - 2 * playerWidht - ballSize}px`;
   right.style.top = `${courtHeigth / 2 - playerHeigth / 2}px`;
   left.style.top = `${courtHeigth / 2 - playerHeigth / 2}px`;
-  ball.style.top = `${Math.round(Math.random() * courtHeigth)}px`; //generate random
+  ball.style.top = `${Math.round(Math.random() * courtHeigth)}px`;
   ball.style.left = `${20}px`;
   stillPlaying = true;
+  keyPressed = "";
   ballStepper();
-  document.addEventListener("keydown", humanPlayer);
+  document.addEventListener("keydown", setKeyPressed);
+  document.addEventListener("keyup", setKeyPressed);
+  playerInterval = setInterval(humanPlayer, playersTime);
   computerPlayer();
 };
 
 const ballStepper = function () {
-  const time = 15;
   let hSense = 1;
   let vSense = 1;
-  const hBallStep = 4;
-  const vBallStep = hBallStep / 2;
   const hMin = 0;
   const hMax = courtWidth - ballSize - 2 * playerWidht;
   const vMin = 0;
@@ -41,20 +47,25 @@ const ballStepper = function () {
   ballInterval = setInterval(async function () {
     let initTop = Math.abs(parseInt(ball.style.top.slice(0, -2)));
     let initLeft = Math.abs(parseInt(ball.style.left.slice(0, -2)));
-    let vRightMin = Math.abs(parseInt(right.style.top.slice(0, -2)) - ballSize);
+    let vRightMin = Math.abs(parseInt(right.style.top.slice(0, -2)));
     let vRightMax = Math.abs(parseInt(right.style.top.slice(0, -2))) + playerHeigth;
-    let vLeftMin = Math.abs(parseInt(left.style.top.slice(0, -2)) - ballSize);
+    let vLeftMin = Math.abs(parseInt(left.style.top.slice(0, -2)));
     let vLeftMax = Math.abs(parseInt(left.style.top.slice(0, -2))) + playerHeigth;
+
     // Point
-    if (initLeft >= hMax && (initTop <= vRightMin || initTop >= vRightMax)) {
+    if (initLeft > hMax && (initTop + ballSize < vRightMin || initTop > vRightMax)) {
       return await score("left");
     }
-    if (initLeft <= hMin && (initTop <= vLeftMin || initTop >= vLeftMax)) {
+    if (initLeft < hMin && (initTop + ballSize < vLeftMin || initTop > vLeftMax)) {
       return await score("right");
     }
+
     // Heat or court limits
     if (stillPlaying) {
-      if (initLeft <= hMin || initLeft >= hMax) {
+      if (
+        (initLeft <= hMin || initLeft >= hMax) &&
+        (initLeft - hSense * hBallStep > hMin || initLeft - hSense * hBallStep < hMax)
+      ) {
         hSense = -hSense;
       }
       if (initTop <= vMin || initTop >= vMax) {
@@ -69,17 +80,15 @@ const ballStepper = function () {
         ball.style.left = `${initLeft + hSense * hBallStep}px`;
       }
     }
-  }, time);
+  }, ballTime);
 };
 
 const humanPlayer = function (event) {
-  const keyName = event.key;
-  if (keyName == "ArrowUp" || keyName == "ArrowDown") {
-    event.preventDefault();
-    const playerStep = 20;
+  if (keyPressed == "ArrowUp" || keyPressed == "ArrowDown") {
+    const playerStep = 2;
     const topInit = Math.abs(parseInt(right.style.top.slice(0, -2)));
     let sense = 1;
-    if (keyName == "ArrowUp") {
+    if (keyPressed == "ArrowUp") {
       sense = -1;
     }
     let stepLength = topInit + sense * playerStep;
@@ -94,21 +103,13 @@ const humanPlayer = function (event) {
 };
 
 const computerPlayer = function () {
-  const time = 15;
-  const playerStep = 20;
+  let stepLength;
   computerInterval = setInterval(() => {
     let topInit = Math.abs(parseInt(left.style.top.slice(0, -2)));
     let ballTop = Math.abs(parseInt(ball.style.top.slice(0, -2)));
-    let sense = 1;
-    let s = 0.3;
-    if (topInit - s * playerHeigth < ballTop && topInit + (1 + s) * playerHeigth >= ballTop) {
-      sense = 0;
-    } else if (topInit + playerHeigth / 2 <= ballTop) {
-      sense = 1;
-    } else {
-      sense = -1;
-    }
-    let stepLength = topInit + sense * playerStep;
+
+    stepLength = (ballTop - playerHeigth / 2 - topInit) * hard + topInit;
+
     if (stepLength < 0) {
       left.style.top = `${0}px`;
     } else if (stepLength > courtHeigth - playerHeigth) {
@@ -116,14 +117,15 @@ const computerPlayer = function () {
     } else if (stepLength >= 0 || stepLength <= courtHeigth - playerHeigth) {
       left.style.top = `${stepLength}px`;
     }
-  }, time);
+  }, playersTime);
 };
 
 const score = async function (player) {
-  console.log("score", player);
-  document.removeEventListener("keydown", humanPlayer);
+  document.removeEventListener("keydown", setKeyPressed);
+  document.removeEventListener("keyup", setKeyPressed);
   clearInterval(ballInterval);
   clearInterval(computerInterval);
+  clearInterval(playerInterval);
   let ballColor;
   for (let i = 0; i < 11; i++) {
     if (i % 2) {
@@ -141,8 +143,7 @@ const score = async function (player) {
     leftScore.textContent = parseInt(leftScore.textContent) + 1;
   }
   await sleep(500);
-  console.log("parseInt(leftScore.textContent)", parseInt(leftScore.textContent));
-  if (parseInt(leftScore.textContent) >= 5) {
+  if (parseInt(leftScore.textContent) >= 5 || parseInt(rightScore.textContent) >= 5) {
     return null;
   } else {
     start();
@@ -153,6 +154,17 @@ const sleep = function (time) {
   return new Promise((resolve) => {
     setTimeout(resolve, time);
   });
+};
+
+const setKeyPressed = function (event) {
+  if (event.key == "ArrowUp" || event.key == "ArrowDown") {
+    event.preventDefault();
+    if (event.type == "keyup") {
+      return (keyPressed = "");
+    } else if (event.type == "keydown") {
+      return (keyPressed = event.key);
+    }
+  }
 };
 
 start();
